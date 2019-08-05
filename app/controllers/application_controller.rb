3227ed323dd4cc22b0ddf2e77d1554b95c464147
qchuchu <quentin.churet@outlook.fr>
2019-08-05 20:48:04 +0200

    Final Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
+
+  private
+
+   def set_current_user
+     session[:username] = 'Visiteur' if session[:username].nil?
+   end
 end
